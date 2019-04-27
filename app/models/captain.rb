ae44085b29d0f94f25d8efed43d1698ba2eec6c6
Jonathan Luk <jluk87@gmail.com>
2019-04-27 22:25:28 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -19,7 +19,7 @@ class Captain < ActiveRecord::Base
     where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
 
   def self.non_sailors
-    includes(boats: :classifications).where("classifications.name <> ?","Sailboat").distinct  
+    where.not("id IN (?)", self.sailors.pluck(:id))
   end
   
 end
